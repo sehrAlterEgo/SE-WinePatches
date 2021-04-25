62680bb76d09ac8afc1014e452ef9143a016d301
filip <fdragovci@gmail.com>
2021-04-26 01:44:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# SE-WinePatches
+Patching some unimplemented wine calls.
+
+
+## Build
+
+```sh
+sudo apt install mono-complete
+xbuild /p:Configuration=Release
+```
+
+## Usage
+
+* copy geoplugin.dll and Harmony0.dll (from bin/release) to your game dir and run
+```sh
+steam -applaunch 244850 -plugin geoplugin.dll
+```
---
+++ geoplugin/PatchGeoInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Sandbox;
+using Sandbox.Engine.Analytics;
+using VRage.Plugins;
+
+namespace PatchGeoInfoPlugin
+{
+	public class PatchGeoInfo : IPlugin
+    {
+	    public void Init(object gameInstance)
+        {
+             Harmony harmony = new Harmony("Test");
+             MySandboxGame.Log.WriteLine("Initializing Wine Patcher Plugin...");
+
+             Type targetType = AccessTools.TypeByName("VRage.Platform.Windows.Win32.WinApi");
+             if (targetType == null)
+                 throw new Exception("Unable to find WinApi");
+             Type targetParamType = targetType.GetNestedType("GeoTypeEnum");
+             if (targetParamType == null)
+                 throw new Exception("Unable to find GeoTypeEnum");
+             MethodInfo target = AccessTools.DeclaredMethod(targetType, "GetGeoInfo", new Type[] { targetParamType });
+             if (target == null)
+                 throw new Exception("Unable to find GetGeoInfo");
+             HarmonyMethod prefix = new HarmonyMethod(typeof(PatchGeoInfo), "Patch_GetGeoInfo");
+             //typeof(PatchGeoInfoPlugin).GetMethod("Patch_GetGeoInfo", BindingFlags.Static | BindingFlags.Public);
+             if (prefix == null)
+                 throw new Exception("Unable to find Patch_GetGeoInfo");
+             harmony.Patch(target, prefix);
+             MySandboxGame.Log.WriteLine("GeoInfo Patched.");
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+        public static bool Patch_GetGeoInfo(ref string __result)
+        {
+            __result = "0";
+            return false;
+        }
+    }
+}
